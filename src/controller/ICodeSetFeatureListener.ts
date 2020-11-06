42017082ded826dfd82ce3781913bf9b84399d18
samgoldstein <goldstein.sa@husky.neu.edu>
2020-11-05 19:38:39 -0500

    Updated comments

--- src/controller/ICodeSetFeatureListener.ts
+++ src/controller/ICodeSetFeatureListener.ts
@@ -1,6 +1,25 @@
+/**
+ * Interface for CodeSet, to be implemented by CodeSetController
+ */
 export default interface ICodeSetFeatureListener {
+    /**
+     * Called when needed to add a program to the codeset
+     */
     onAddProgram()
+    /**
+     * Called when needed to delete a program from the codeset
+     * @param programName name of program to be deleted
+     */
     onDeleteProgram(programName: string)
+    /**
+     * Called when needed to delete a file from the codeset
+     * May need to check for if filename exists within program name
+     * @param programName name of program with file to be deleted
+     * @param fileName name of file to be deleted
+     */
     onDeleteFile(programName: string, fileName: string)
+    /**
+     * Called when needed to generate report
+     */
     onGenerateReport()
 }
