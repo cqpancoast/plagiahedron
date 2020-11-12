7b69a782ece931c14780068204f9113961245184
Casey Pancoast <caseypancoast@Greg.local>
2020-11-12 17:14:21 -0500

    added bones to PHFile, IdentityParser skeletons

--- test/model/IdentityParser.spec.ts
+++ test/model/IdentityParser.spec.ts
@@ -1,57 +1,27 @@
 import PHFile from "../../src/model/PHFile"
 
 /**
- * Hello, and welcome to the ONLY complicated test file so far.
- * We'll decide tomorrow on the form of the parser we'll actually use, and I'll make that then.
- * For now, we should test this because we may use this in other tests.
- * Also, making this helped me conceptually in making a more general parser,
- * so maybe it's the same thing for you testing it!
+ * @Sam: These are additional tests in addition to the ones you've already done, not replacements.
  */
 
-describe("parse a basic PHFile", () => {
+
+describe("unparsing and finding file similarities with incorrect parse types (undefined or non-string)", () => {
 
     let file: PHFile = new PHFile("f", "ts", "firstline\nsecondline\nthirdline")
 
-    it("identity parser simply returns the file contents", () => {
-
-    })
-})
-
-describe("unparse a basic PHFile", () => {
-
-    let stringFirst: string = "firstline"
-    let stringLine: string = "line"
-    let stringDline: string = "dline"
-    let file: PHFile = new PHFile("f", "ts", "firstline\nsecondline\nthirdline")
-
-    it("finds one instance of firstline", () => {
+    it("errors when attemtping to unparse numeric parse type", () => {
 
     })
 
-    it("finds three instances of line", () => {
+    it("errors when attemtping to unparse unset parse type", () => {
 
     })
 
-    it("finds two instances of dline", () => {
-
-    })
-})
-
-describe("find parsed matches between a basic PHFile", () => {
-
-    let file123: PHFile = new PHFile("f", "ts", "firstline\nsecondline\nthirdline")
-    let file2: PHFile = new PHFile("f", "ts", "secondline")
-    let file13: PHFile = new PHFile("f", "ts", "firstline\nthirdline")
-    
-    it("finds one match between a file and itself", () => {
+    it("errors on find similarities between files of numeric parse type", () => {
 
     })
 
-    it("finds one match between a file and a substring file of itself", () => {
-
-    })
-
-    it("finds two matches between a file and a file made of two substrings of itself", () => {
+    it("errors on find similarities between files of unset parse type", () => {
 
     })
 })
--- test/model/PHFile.spec.ts
+++ test/model/PHFile.spec.ts
@@ -1,24 +1,35 @@
 import PHFile from "../../src/model/PHFile"
 import IdentityParser from "../../src/model/IdentityParser"
+import NumberParser from "./NumberParser"
 
-describe("test non-parsed PHFile", () => {
+/**
+ * @Sam: These are additional tests in addition to the ones you've already done, not replacements.
+ */
+
+describe("test errors on getting/setting program name", () => {
 
     let file: PHFile = new PHFile("f", "ts", "firstline\nsecondline\nthirdline")
 
-    /** Test getters, program name set/get rules, error when parsed content accessed (implementation TODO) */
+    /** Make two programs, check that the getter works correctly
+     * when you first add this to a program's constructor, 
+     * check that it errors the second time.*/
 
     it("", () => {
 
     })
 })
 
-describe("test parsed PHFile", () => {
+describe("test errors when accepting first and second parser", () => {
 
     let file: PHFile = new PHFile("f", "ts", "firstline\nsecondline\nthirdline")
     let idParser: IdentityParser = new IdentityParser(4)
+    let numParser: NumberParser = new NumberParser()
     file.acceptParser(idParser)
 
-    /** Test getters, parsed content (identical to file contents in this case) */
+    /** Make two parsers, check that the accepter works correctly
+     * when the PHFile first accepts a parser, 
+     * check that it errors the second time.
+     * Also test that it errors for the SAME parser. */
 
     it("", () => {
 
