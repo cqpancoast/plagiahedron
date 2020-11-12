93bacb87fda068d6b337081124e15d0a7be6c7f2
Casey Pancoast <caseypancoast@Greg.local>
2020-11-12 17:43:25 -0500

    added IParser error on different file types

--- test/model/IdentityParser.spec.ts
+++ test/model/IdentityParser.spec.ts
@@ -2,10 +2,11 @@ import PHFile from "../../src/model/PHFile"
 
 /**
  * @Sam: These are additional tests in addition to the ones you've already done, not replacements.
+ * Please also add an additional test to your suite that errors if the files being compared in
+ * IDParser.findParsedMatches have different extensions.
  */
 
-
-describe("unparsing and finding file similarities with incorrect parse types (undefined or non-string)", () => {
+ describe("unparsing and finding file similarities with incorrect parse types (undefined or non-string)", () => {
 
     let file: PHFile = new PHFile("f", "ts", "firstline\nsecondline\nthirdline")
 
