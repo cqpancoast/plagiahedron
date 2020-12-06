770f8a795e90c848974f57210ed81c1681ff2f20
maxabisia <abisia.m@husky.neu.edu>
2020-12-06 13:04:18 -0500

    added ConversionUtils

---
+++ team-01/src/model/ConversionUtils.ts
@@ -0,0 +1,23 @@
+
+import CodeSet from './CodeSet'
+import Program from './Program'
+import PHFile from './PHFile'
+
+export default class ConversionUtils {
+
+    // Takes File filtype and converts to PHFile
+    static convertFileToPHFile(file: File): PHFile | null {
+        return null
+    }
+
+    // Creates a new Program from an array of PHFiles
+    static makeProgramFromFiles(files: PHFile[]): Program | null {
+        return null
+    }
+
+    // Adds Program to CodeSet
+    static uploadProgramToCodeSet(program: Program): void {
+
+    }
+
+}
